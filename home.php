25cf3650af2863335df44f67933ae750ee007d14
jyotit2015 <jyotit@smartdatainc.net>
2019-09-13 02:05:06 +0530

    Add files via upload

--- home.php
+++ home.php
@@ -1,77 +1,122 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-    
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('home_model');
-        $this->load->library('form_validation');
-    }
+<?php include 'header.php'; ?>
+<div class="row clear_fix">
+    <div class="col-md-12">
 
-    public function index()
-	{   
-        $this->load->view('home');
-	}
-        
-        public function fillgrid(){
-            $this->home_model->fillgrid();
-        }
+        <div id="response"></div>
 
+        <div class="well">
+            <form class="form-inline" role="form" id="frmadd" action="<?php echo base_url() ?>home/create" method="POST">
+                <div class="form-group">
+                    <label class="sr-only" for="exampleInputEmail2">Full name</label>
+                    <input type="text" name="name" class="form-control" id="exampleInputEmail2" placeholder="name">
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <div class="input-group-addon">@</div>
+                        <input class="form-control" name="email" type="email" placeholder="Enter email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="exampleInputPassword2">Contact</label>
+                    <input type="text" class="form-control" name="contact" id="exampleInputPassword2" placeholder="contact number">
+                </div>
+                <div class="form-group">
+                    <label class="sr-only" for="exampleInputPassword2">facebook link</label>
+                    <input type="text" name="facebook" class="form-control" id="exampleInputPassword2" placeholder="http://facebook.com/pariharvikram1989">
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-success" id="exampleInputPassword2" value="submit">
+                </div>
+            </form>
+        </div>
 
-        public function create(){
-            $this->form_validation->set_rules('name', 'Name', 'required');
-            $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-            $this->form_validation->set_rules('contact', 'Contact Number', 'required|numeric|max_length[10]|min_length[10]');
-            if ($this->form_validation->run() == FALSE){
-               echo'<div class="alert alert-danger">'.validation_errors().'</div>';
-               exit;
-            }
-            else{
-                $this->home_model->create();
-            }
-        }
-        
-        public function edit(){
-            $id =  $this->uri->segment(3);
-            $this->db->where('id',$id);
-            $data['query'] = $this->db->get('curd');
-            $data['id'] = $id;
-            $this->load->view('edit', $data);
-            }
+        <table class="table">
+            <thead><tr><th>Name</th><th>Email</th><th>Contact</th><th>facebook link</th><th>created</th><th>Action</th></tr></thead>
+            <tbody id="fillgrid">
             
-        public function update(){
-                $res['error']="";
-                $res['success']="";
-                $this->form_validation->set_rules('name', 'Name', 'required');
-                $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-                $this->form_validation->set_rules('contact', 'Contact Number', 'required|numeric|max_length[10]|min_length[10]');
-                if ($this->form_validation->run() == FALSE){
-                $res['error']='<div class="alert alert-danger">'.validation_errors().'</div>';    
-                }           
-            else{
-                $data = array('name'=>  $this->input->post('name'),
-                'email'=>$this->input->post('email'),
-                'contact'=>$this->input->post('contact'),
-                'facebook_link'=>$this->input->post('facebook'));
-                $this->db->where('id', $this->input->post('hidden'));
-                $this->db->update('curd', $data);
-                $data['success'] = '<div class="alert alert-success">One record inserted Successfully</div>';
-            }
-            header('Content-Type: application/json');
-            echo json_encode($res);
-            exit;
-        }
+            </tbody>
+            <tfoot></tfoot>
+        </table>
 
 
-        public function delete(){
-            $id =  $this->input->POST('id');
-            $this->db->where('id', $id);
-            $this->db->delete('curd');
-            echo'<div class="alert alert-success">One record deleted Successfully</div>';
-            exit;
-        }
-        
-}
+    </div>
+</div>
+</div>
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+
+
+<script>
+$(document).ready(function (){
+    //fill data
+    var btnedit='';
+    var btndelete = '';
+        fillgrid();
+        // add data
+        $("#frmadd").submit(function (e){
+            e.preventDefault();
+            $("#loader").show();
+            var url = $(this).attr('action');
+            var data = $(this).serialize();
+            $.ajax({
+                url:url,
+                type:'POST',
+                data:data
+            }).done(function (data){
+                $("#response").html(data);
+                $("#loader").hide();
+                fillgrid();
+            });
+        });
+    
+    
+    
+    function fillgrid(){
+        $("#loader").show();
+        $.ajax({
+            url:'<?php echo base_url() ?>home/fillgrid',
+            type:'GET'
+        }).done(function (data){
+            $("#fillgrid").html(data);
+            $("#loader").hide();
+            btnedit = $("#fillgrid .btnedit");
+            btndelete = $("#fillgrid .btndelete");
+            var deleteurl = btndelete.attr('href');
+            var editurl = btnedit.attr('href');
+            //delete record
+            btndelete.on('click', function (e){
+                e.preventDefault();
+                var deleteid = $(this).data('id');
+                if(confirm("are you sure")){
+                    $("#loader").show();
+                    $.ajax({
+                    url:deleteurl,
+                    type:'POST' ,
+                    data:'id='+deleteid
+                    }).done(function (data){
+                    $("#response").html(data);
+                    $("#loader").hide();
+                    fillgrid();
+                    });
+                }
+            });
+            
+            //edit record
+            btnedit.on('click', function (e){
+                e.preventDefault();
+                var editid = $(this).data('id');
+                $.colorbox({
+                href:"<?php echo base_url()?>home/edit/"+editid,
+                top:50,
+                width:500,
+                onClosed:function() {fillgrid();}
+                });
+            });
+            
+        });
+    }
+    
+});
+</script>
+</body>
+</html>
